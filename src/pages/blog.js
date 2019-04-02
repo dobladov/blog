7397eb0ff87c86049cb612a7de2762297e08a1d1
Daniel Doblado <danieldoblado@gmail.com>
2019-04-02 16:41:35 +0200

    Create blog list of posts

---
+++ src/pages/blog.js
@@ -0,0 +1,50 @@
+import React from "react"
+import { StaticQuery, graphql, Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const SecondPage = () => (
+  <StaticQuery
+  query={graphql`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              title
+              path
+              date
+            }
+          }
+        }
+      }
+    }
+  `}
+  render={data => {
+    const posts = data.allMarkdownRemark.edges
+
+    return (
+    <Layout>
+      <SEO title="Page two" />
+      <h1>List of blog posts</h1>
+      <ul>
+        {posts.map(post => (
+          <li key={post.node.frontmatter.path}>
+          <Link
+            to={post.node.frontmatter.path}
+          >
+            {post.node.frontmatter.title}
+          </Link> <br/>
+          </li>
+        ))}
+      </ul>
+      <br/>
+      <Link to="/">Go back to the homepage</Link>
+
+    </Layout>
+  )}}
+/>
+)
+
+export default SecondPage
