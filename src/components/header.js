3712695be6917d35ad91029428ce09dffa12caaa
Daniel Doblado <danieldoblado@gmail.com>
2019-04-03 00:02:38 +0200

    Refactor header

--- src/components/header.js
+++ src/components/header.js
@@ -1,32 +1,57 @@
+/** @jsx jsx **/
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
-import React from "react"
+import { jsx, css } from '@emotion/core'
+import { colors as c } from "../style/variables"
+
+const style = css`
+  padding: 15px;
+  display: flex;
+  align-items: center;
+
+  h1 {
+    margin: 0;
+    padding: 0;
+    font-size: 1rem;
+  }
+
+  &:hover {
+    a {
+      opacity: 1;
+    }
+  }
+
+
+  a {
+    transition: color .2s ease-in-out;
+    color: ${c.text};
+    opacity: .6;
+    text-decoration: none;
+    marginBottom: 1.45rem;
+
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+
+  .headerLinks {
+    margin-left: 20px;
+  }
+`
 
 const Header = ({ siteTitle }) => (
   <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
+    css={style}
   >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
+    <Link
+      to="/"
     >
       <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
+        {siteTitle}
       </h1>
+    </Link>
+    <div className="headerLinks">
+      <Link to="/blog">Blog</Link>
     </div>
   </header>
 )
