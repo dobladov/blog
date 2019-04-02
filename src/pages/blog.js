be331ff9cb6c9f6f91baf08ebd64b5d1c76a2700
Daniel Doblado <danieldoblado@gmail.com>
2019-04-02 17:08:04 +0200

    Add emotion css

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,8 +1,17 @@
-import React from "react"
+/** @jsx jsx */
+// import React from "react"
 import { StaticQuery, graphql, Link } from "gatsby"
+import { css, jsx } from '@emotion/core'
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+
+const style = css`
+  ul {
+    list-style-type: circle;
+    padding: 0;
+  };
+`
 
 const SecondPage = () => (
   <StaticQuery
@@ -27,25 +36,26 @@ const SecondPage = () => (
     return (
     <Layout>
       <SEO title="Page two" />
-      <h1>List of blog posts</h1>
-      {posts.length > 0 ? (
-        <ul>
-          {posts.map(post => (
-            <li key={post.node.frontmatter.path}>
-            <Link
-              to={post.node.frontmatter.path}
-            >
-              {post.node.frontmatter.title}
-            </Link> <br/>
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <div>No posts yet</div>
-      )}
-      <br/>
-      <Link to="/">Go back to the homepage</Link>
-
+      <div css={style}>
+        <h1>List of blog posts</h1>
+        {posts.length > 0 ? (
+          <ul>
+            {posts.map(post => (
+              <li key={post.node.frontmatter.path}>
+              <Link
+                to={post.node.frontmatter.path}
+              >
+                {post.node.frontmatter.title}
+              </Link> <br/>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <div>No posts yet</div>
+        )}
+        <br/>
+        <Link to="/">Go back to the homepage</Link>
+      </div>
     </Layout>
   )}}
 />
